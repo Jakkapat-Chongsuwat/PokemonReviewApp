1850326bbbf3158f46335eb0d7634f60b0856a6e
Jakkapat-Chongsuwat <70486906+Jakkapat-Chongsuwat@users.noreply.github.com>
2024-01-30 16:02:20 +0700

    Add docker template for development

--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -23,6 +23,7 @@ builder.Services.AddScoped<IReviewerRepository, ReviewerRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
